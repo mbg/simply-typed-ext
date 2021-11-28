a1e8e5cda027ef0b555546125c80f86d594710d7
Finnbar Keating <finnjkeating@gmail.com>
2021-11-28 21:27:29 +0000

    Removed Literate Haskell

--- src/Token.hs
+++ src/Token.hs
@@ -3,29 +3,29 @@
                                        Michael Benjamin Gale                                       
 --------------------------------------------------------------------------------------------------}
 
-> module Token (
->   Token(..)
-> ) where
+module Token (
+    Token(..)
+) where
 
->   data Token = TVar String
->              | TType String
->              | TVal Int
->              | TSucc
->              | TPred
->              | TIf
->              | TThen
->              | TElse
->              | TFix
->              | TIsZero
->              | TLet
->              | TAbs
->              | TDot
->              | TOpen
->              | TClose 
->              | TColon
->              | TArrow
->              | TEquals
->              deriving Show
+data Token = TVar String
+           | TType String
+           | TVal Int
+           | TSucc
+           | TPred
+           | TIf
+           | TThen
+           | TElse
+           | TFix
+           | TIsZero
+           | TLet
+           | TAbs
+           | TDot
+           | TOpen
+           | TClose 
+           | TColon
+           | TArrow
+           | TEquals
+           deriving Show
                
 
 {--------------------------------------------------------------------------------------------------
